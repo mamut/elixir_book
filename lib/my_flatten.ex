21d3330134202bd0cddbb9ca73639be945ebace3
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-04-10 22:37:54 +0200

    tests for flatten

---
+++ lib/my_flatten.ex
@@ -0,0 +1,18 @@
+defmodule MyFlatten do
+  def flatten([]), do: []
+
+  def flatten(list) do
+    Enum.reverse do_flatten(list, [])
+  end
+
+  defp do_flatten([], agg), do: agg
+
+  defp do_flatten([head|tail], agg) do
+    if is_list(head) do
+      do_flatten(tail, do_flatten(head, agg))
+    else
+      do_flatten(tail, [head | agg])
+    end
+  end
+end
+
---
+++ test/my_flatten_test.exs
@@ -0,0 +1,19 @@
+defmodule MyFlattenTest do
+  use ExUnit.Case
+
+  test "flattening empty list" do
+    assert MyFlatten.flatten([]) == []
+  end
+
+  test "flattening list with no sublists" do
+    assert MyFlatten.flatten([1, 2, 3, 4]) == [1, 2, 3, 4]
+  end
+
+  test "flattening list with single sublist" do
+    assert MyFlatten.flatten([1, 2, 3, [4]]) == [1, 2, 3, 4]
+  end
+
+  test "flattening list with nested sublists" do
+    assert MyFlatten.flatten([ 1, [ 2, 3, [4] ], 5, [[[6]]]]) == [1, 2, 3, 4, 5, 6]
+  end
+end
