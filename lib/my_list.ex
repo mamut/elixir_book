a6f64cc4cf88c1c14d1e89ee33a196ffaab72c00
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-04-10 23:10:44 +0200

    Tests for MyList

--- lib/my_list.ex
+++ lib/my_list.ex
@@ -1,4 +1,4 @@
-defmodule Listlist do
+defmodule MyList do
   def all?([], _), do: true
   def all?([head | tail], func), do: func.(head) && all?(tail, func)
 
@@ -31,20 +31,3 @@ defmodule Listlist do
   def do_take([head|tail], n, agg), do: do_take(tail, n-1, [head|agg])
   def do_take_result(agg), do: Enum.reverse(agg)
 end
-
-list = Enum.to_list 1..5
-
-IO.inspect Listlist.all?(list, &(&1 < 4))
-IO.inspect Listlist.all?([], &(&1 < 4))
-
-IO.inspect Listlist.each(list, &IO.puts/1)
-
-IO.inspect Listlist.filter(list, &(&1 > 2))
-
-IO.inspect Listlist.split(list, 3)
-IO.inspect Listlist.split(list, 0)
-IO.inspect Listlist.split([], 3)
-
-IO.inspect Listlist.take(list, 3)
-IO.inspect Listlist.take(list, 0)
-IO.inspect Listlist.take([], 3)
---
+++ test/my_list_test.exs
@@ -0,0 +1,55 @@
+defmodule MyListTest do
+  use ExUnit.Case
+
+  import ExUnit.CaptureIO
+
+  def list, do: Enum.to_list(1..5)
+
+  test "all? checks a predicate" do
+    assert MyList.all?(list, &(&1 < 4)) == false
+  end
+
+  test "all? makes sense of empty lists" do
+    assert MyList.all?([], &(&1 < 4)) == true
+  end
+
+  test "each calls a function once for each element in the list" do
+    assert capture_io(fn ->
+      MyList.each(list, &IO.puts/1)
+    end) == """
+    1
+    2
+    3
+    4
+    5
+    """
+  end
+
+  test "filter lets through only elements that pass the predicate" do
+    assert MyList.filter(list, &(&1 > 2)) == [3, 4, 5]
+  end
+
+  test "split cuts a list in specified position" do
+    assert MyList.split(list, 3) == [[1, 2, 3], [4, 5]]
+  end
+
+  test "split can happen at the very beginning" do
+    assert MyList.split(list, 0) == [[], [1, 2, 3, 4, 5]]
+  end
+
+  test "split can be used on empty list" do
+    assert MyList.split([], 3) == [[], []]
+  end
+
+  test "take returns a part of the list" do
+    assert MyList.take(list, 3) == [1, 2, 3]
+  end
+
+  test "take can also take nothing from a list" do
+    assert MyList.take(list, 0) == []
+  end
+
+  test "take can be called on an empty list" do
+    assert MyList.take([], 3) == []
+  end
+end
