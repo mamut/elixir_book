164735c21abecfb55bc7b054dd11e7da7e2f015d
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-12-04 22:08:44 +0100

    Tests for comprehensions

---
+++ lib/comprehension.ex
@@ -0,0 +1,22 @@
+defmodule Comprehension do
+  @moduledoc "Calculates primes and taxes."
+
+  def primes(n) do
+    list = for x <- 2..n, do: [x, Fromto.span(2, div(x,2))]
+    list
+      |> Enum.filter(&prime?/1)
+      |> Enum.map(&List.first/1)
+  end
+
+  def prime?([x, divisors]) do
+    Enum.all?(divisors, &(rem(x, &1) != 0))
+  end
+
+  def tax(orders, tax_rates) do
+    for order <- orders, ship_to = order[:ship_to] do
+      tax_rate = Keyword.get(tax_rates, ship_to, 0.0)
+      total_amount = order[:net_amount] * (1 + tax_rate)
+      Keyword.put order, :total_amount, total_amount
+    end
+  end
+end
