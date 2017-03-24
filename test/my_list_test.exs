3cf6f84e020939e9ad3508a1932535d11a9d7a52
Krzysztof Urban <krzysztof.urban@netguru.pl>
2017-03-24 11:40:29 +0100

    fix warnings

--- test/my_list_test.exs
+++ test/my_list_test.exs
@@ -6,7 +6,7 @@ defmodule MyListTest do
   def list, do: Enum.to_list(1..5)
 
   test "all? checks a predicate" do
-    refute MyList.all?(list, &(&1 < 4))
+    refute MyList.all?(list(), &(&1 < 4))
   end
 
   test "all? makes sense of empty lists" do
@@ -15,7 +15,7 @@ defmodule MyListTest do
 
   test "each calls a function once for each element in the list" do
     assert capture_io(fn ->
-      MyList.each(list, &IO.puts/1)
+      MyList.each(list(), &IO.puts/1)
     end) == """
     1
     2
@@ -26,15 +26,15 @@ defmodule MyListTest do
   end
 
   test "filter lets through only elements that pass the predicate" do
-    assert MyList.filter(list, &(&1 > 2)) == [3, 4, 5]
+    assert MyList.filter(list(), &(&1 > 2)) == [3, 4, 5]
   end
 
   test "split cuts a list in specified position" do
-    assert MyList.split(list, 3) == [[1, 2, 3], [4, 5]]
+    assert MyList.split(list(), 3) == [[1, 2, 3], [4, 5]]
   end
 
   test "split can happen at the very beginning" do
-    assert MyList.split(list, 0) == [[], [1, 2, 3, 4, 5]]
+    assert MyList.split(list(), 0) == [[], [1, 2, 3, 4, 5]]
   end
 
   test "split can be used on empty list" do
@@ -42,11 +42,11 @@ defmodule MyListTest do
   end
 
   test "take returns a part of the list" do
-    assert MyList.take(list, 3) == [1, 2, 3]
+    assert MyList.take(list(), 3) == [1, 2, 3]
   end
 
   test "take can also take nothing from a list" do
-    assert MyList.take(list, 0) == []
+    assert MyList.take(list(), 0) == []
   end
 
   test "take can be called on an empty list" do
